b95dda42d0bd819248141927c8871e2650996a9d
Yong Joon Lee <112607019+infected4098@users.noreply.github.com>
2025-07-31 17:41:27 +0900

    Create bigvgan_utils.py

---
+++ bigvgan_utils.py
@@ -0,0 +1,47 @@
+import glob
+import os
+import torch
+from torch.nn.utils import weight_norm
+from fvcore.nn import FlopCountAnalysis, parameter_count_table
+from torchinfo import summary
+import scipy.signal
+import shutil
+from torch.autograd import grad
+from torchaudio.transforms import Resample
+
+import torch.nn.functional as F
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def build_env(config, config_name, path):
+    t_path = os.path.join(path, config_name)
+    if config != t_path:
+        os.makedirs(path, exist_ok=True)
+        shutil.copyfile(config, os.path.join(path, config_name))
+
+
+def load_checkpoint(filepath, device):
+    assert os.path.isfile(filepath)
+    print("Loading '{}'".format(filepath))
+    checkpoint_dict = torch.load(filepath, map_location=device)
+    print("Complete.")
+    return checkpoint_dict
+
+
+def save_checkpoint(filepath, obj):
+    print("Saving checkpoint to {}".format(filepath))
+    torch.save(obj, filepath)
+    print("Complete.")
+
+
+def scan_checkpoint(cp_dir, prefix):
+    pattern = os.path.join(cp_dir, prefix + '????????')
+    cp_list = glob.glob(pattern)
+    if len(cp_list) == 0:
+        return None
+    return sorted(cp_list)[-1]
